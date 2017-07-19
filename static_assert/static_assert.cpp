99a8034ddd585ec2eb9adf180fc832cf706ea43c
lolisia <lolisia@gmail.com>
2017-07-19 23:39:11 +0900

    add static_assert optional message example

---
+++ static_assert/static_assert.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+// need additional compile option "/std:c++latest" for static_assert
+// https://docs.microsoft.com/en-us/cpp/error-messages/compiler-errors-1/compiler-error-c2429
+
+struct no_default_constructor
+{
+	no_default_constructor() = delete;
+};
+
+template<typename T> struct check_default_constructor
+{
+	static_assert(std::is_default_constructible<T>::value, "no default constructor");	// C++11 static_assert with message
+	static_assert(std::is_default_constructible<T>::value);								// C++17 static_assert without message
+};
+
+int main()
+{
+	//check_default_constructor<no_default_constructor> error;		// compile error!
+
+	return 0;
+}
